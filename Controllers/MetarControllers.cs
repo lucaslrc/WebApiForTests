028917d6eeb35f270f3dc59a5e89812f86522ef9
Lucas Ribeiro da Costa <lucas.costa@benner.com.br>
2019-11-27 09:31:18 -0300

    Working

---
+++ Controllers/MetarControllers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApiForTests.Models;
+using WebApiForTests.Services;
+
+namespace WebApiForTests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MetarController : ControllerBase
+    {
+        [HttpPost("decoder")]
+        public string Get([FromBody]MetarModel model)
+        {
+            if (string.IsNullOrEmpty(model.Metar))
+            {
+                return "Metar vazio, por favor tente novamente.";
+            }
+            else
+            {
+                var ALW = new AirportListWeather();
+                
+                return ALW.GetWeatherInfo(model.Metar);
+            }
+        }
+    }
+}
