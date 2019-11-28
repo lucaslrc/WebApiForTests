cc258c02dc132a3038ec8a0d12662c872e206122
Lucas Ribeiro da Costa <lrc@rivendell.arda>
2019-11-27 23:09:30 -0300

    Fazendo todo o processo para traduzir

--- Services/TranslateMetar.cs
+++ Services/TranslateMetar.cs
@@ -1,15 +1,47 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Net;
 
 namespace WebApiForTests.Services
 {
     public class TranslateMetar
     {
-        public string Translate(string Metar)
+        public string Translate(string Icao)
         {
             var a = new AirportListWeather();
 
-            return a.GetWeatherInfo(Metar);
+            return a.GetWeatherInfo(GetMetar(Icao));
+        }
+
+        public string GetMetar(string Icao)
+        {
+            var request = (HttpWebRequest)WebRequest.Create($"http://www.redemet.aer.mil.br/api/consulta_automatica/index.php?local={Icao}&msg=metar");
+ 
+            request.Method = "GET";
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+    
+            var content = string.Empty;
+    
+            using (var response = (HttpWebResponse)request.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(stream))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                }
+            }
+
+            if (content.Contains("não localizada na base de dados da REDEMET"))
+            {
+                return string.Empty + content;
+            }
+            else
+            {
+                return content;
+            }
         }
 
         public string ConvertIcaoForAirportName(string Icao)
