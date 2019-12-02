86e018e2cb5b6425310a9948a76cd05883c94674
Lucas Ribeiro da Costa <lucas.costa@benner.com.br>
2019-12-02 09:15:00 -0300

    Implicacoes no metodo GetWeather foram modificadas

--- Services/AirportListWeather.cs
+++ Services/AirportListWeather.cs
@@ -240,7 +240,14 @@ namespace WebApiForTests.Services
 
                     foreach (Match match in Regex.Matches(variation, pattern, RegexOptions.IgnoreCase))
                     {
-                        resultVariation += $"{item.WeatherInfo} {match.Value.Substring(3, 3)}.\n";
+                        if (String.IsNullOrWhiteSpace(match.Value.Substring(3)))
+                        {
+                            resultVariation += $"{item.WeatherInfo} não informado.\n";
+                        }
+                        else
+                        {
+                            resultVariation += $"{item.WeatherInfo} {match.Value.Substring(3, 3)}.\n";
+                        }
                     }
                 }
             }
